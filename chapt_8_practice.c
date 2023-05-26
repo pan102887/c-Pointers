b2a476dc2c11b20e07589991717a0ddba451b7a7
jupeter <pan102887@qq.com>
2023-05-26 18:20:24 +0800

    chapt 8 practice

--- chapt_8_practice.c
+++ chapt_8_practice.c
@@ -87,10 +87,90 @@ void test_matrix_multiply(void)
     }
 }
 
+int array_offset(int arrayinfo[], ...)
+{
+    int *witch_width = malloc(sizeof(int) * arrayinfo[0]);
+    for (int i = 0; i < arrayinfo[0]; i++)
+    {
+        witch_width[i] = arrayinfo[2 * i + 2] - arrayinfo[2 * i + 1] + 1;
+    }
+
+    int *index = malloc(sizeof(int) * arrayinfo[0]);
+    va_list index_params;
+    va_start(index_params, arrayinfo[0]);
+    int i = 0;
+    while (i < arrayinfo[0])
+    {
+        index[i] = va_arg(index_params, int);
+        i++;
+    }
+    va_end(index_params);
+
+    i = arrayinfo[0] - 1;
+    int loc = (index[i] - arrayinfo[2 * i + 1]);
+    int width = 1;
+    i--;
+    while (!(i < 0))
+    {
+        width *= witch_width[i + 1];
+        loc += (index[i] - arrayinfo[2 * i + 1]) * width;
+        i--;
+    }
+    free(witch_width);
+    free(index);
+    return loc;
+}
+
+void test_array_offset(void)
+{
+    int arrayinfo[7] = {3, 4, 6, 1, 5, -3, 3};
+    int result1 = array_offset(arrayinfo, 4, 1, -3);
+    assert(result1 == 0 || !printf("result actual is: %d\n", result1));
+
+    int result2 = array_offset(arrayinfo, 4, 1, 3);
+    assert(result2 == 6 || !printf("result actual is: %d\n", result2));
+
+    int result3 = array_offset(arrayinfo, 5, 1, -3);
+    assert(result3 == 35 || !printf("result actual is: %d\n", result3));
+
+    int result4 = array_offset(arrayinfo, 4, 1, -2);
+    assert(result4 == 1 || !printf("result actual is: %d\n", result4));
+
+    int result5 = array_offset(arrayinfo, 4, 2, -3);
+    assert(result5 == 7 || !printf("result actual is: %d\n", result5));
+
+    int result6 = array_offset(arrayinfo, 6, 3, 1);
+    assert(result6 == 88 || !printf("result actual is: %d\n", result6));
+}
+
+double average_1(int nnumber, ...)
+{
+    va_list number_params;
+    va_start(number_params, nnumber);
+    double sum = 0;
+    for (int i = 0; i < nnumber; i++)
+    {
+        sum += va_arg(number_params, int);
+    }
+    va_end(number_params);
+    return sum / nnumber;
+}
+
+void test_average(void)
+{
+    double result1 = average_1(3, 1, 2, 3);
+    assert(result1 == 2.0 || !printf("result actual is: %f\n", result1));
+
+    double result2 = average_1(5, 1, 2, 3, 4, 5);
+    assert(result2 == 3.0 || !printf("result actual is: %f\n", result2));
+}
+
 void chapt_8_practice_run(void)
 {
     printf("--------------------chapt_8_practice--------------------\n");
     test_identity_matrix();
     test_matrix_multiply();
+    test_array_offset();
+    test_average();
     printf("====================chapt_8_practice====================\n\n");
 }
--- chapt_8_practice.h
+++ chapt_8_practice.h
@@ -2,6 +2,10 @@
 #define _CHAPTER_8_PRACTICE_H_
 
 #include <stdio.h>
+#include <stdlib.h>
+#include <stdarg.h>
+#include <assert.h>
+#include <string.h>
 
 int identity_matrix(int *mat, int nmat);
 /**
@@ -10,6 +14,22 @@ int identity_matrix(int *mat, int nmat);
  * 这个函数的作用是将 mat1 与 mat2 相乘，结果存储在 r 中，r 是一个 x * z 的矩阵
  */
 int matrix_multiply(int *mat1, int *mat2, int *r, int x, int y, int z);
+/**
+ * @brief 这个函数接受一些用于描述伪数组的维数的信息以及一组下标值。
+ *        然后它使用这些信息把下标值翻译为一个整数，用于表示一个向
+ *        量（一维数组）的下标。
+ *        arrayinfo[0]: 伪数组具有的维数
+ *        arrayinfo[1], arrayinfo[2] 第一维的下标范围（从 arrayinfo[1] 到 arrayinfo[2]）
+ *        arrayinfo[3], arrayinfo[4] 第二维的下标范围（从 arrayinfo[3] 到 arrayinfo[4]）
+ *        以此类推
+ *        行主序
+ *
+ * @param arrayinfo 描述伪数组的维数的信息
+ * @param ... 一组下标值
+ * @return int 用于表示一个向量（一维数组）的下标
+ */
+int array_offset(int arrayinfo[], ...);
+
 void chapt_8_practice_run(void);
 
 #endif
