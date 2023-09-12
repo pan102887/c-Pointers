afdf71351cd3c65986c9638df5f15e35e5a8b9b0
pansf <pan102887@qq.com>
2023-09-12 18:32:54 +0800

    implement m_array_list

--- m_array_list.h
+++ m_array_list.h
@@ -22,9 +22,9 @@ extern m_array_list *m_array_list_new_with_capacity(size_t capacity);
 extern m_array_list *m_array_list_new_with_array(void **array, size_t array_size);
 extern void m_array_list_free(m_array_list *list);
 extern void m_array_list_and_data_free(m_array_list *list, void (*free_element)(void*));
-extern void* get(m_array_list* list, size_t index);
-extern void set(m_array_list* list, size_t index, void* element);
-extern bool add(m_array_list* list, void* element);
+extern void* array_list_get(m_array_list* list, size_t index);
+extern void array_list_set(m_array_list* list, size_t index, void* element);
+extern bool array_list_add(m_array_list* list, void* element);
 
 #if defined(_TEST_)
 extern void array_list_test(void);
