ac7118f61968fb4acc85752effc0774c2fb198dc
pansf <pan102887@qq.com>
2023-10-07 18:30:31 +0800

    tt-tree

--- tt_tree.c
+++ tt_tree.c
@@ -3,45 +3,78 @@
 #include <stdlib.h>
 #include <string.h>
 
+// ========================= struct define ===========================
+
 /* ******** node struct ********* */
-/*|——————————————————————————————|*/
-/*|head:  sizeof(unsigned char)  |*/
-/*|     + sizeof(size_t) * 2     |*/
-/*|——————————————————————————————|*/
-/*|left:  sizeof(void*)          |*/
-/*|——————————————————————————————|*/
-/*|right: sizeof(void*)          |*/
-/*|——————————————————————————————|*/
-/*|midle: sizeof(void*)          |*/
-/*|——————————————————————————————|*/
-/*|data:  sizeof(char*) * 2      |*/
-/*|——————————————————————————————|*/
+/*|————————————————————————————————————————————|*/
+/*|head:                                       |*/
+/*|       node_info_t node_info;               |*/
+/*|       size_t left_key_size;                |*/
+/*|       size_t left_data_size;               |*/
+/*|       size_t right_key_size;               |*/
+/*|       size_t right_data_size;              |*/
+/*|————————————————————————————————————————————|*/
+/*|left:  void *left_child                     |*/
+/*|————————————————————————————————————————————|*/
+/*|right: void *midle_child                    |*/
+/*|————————————————————————————————————————————|*/
+/*|right: void *right_child                    |*/
+/*|————————————————————————————————————————————|*/
+/*|right: void *fourth_child                   |*/
+/*|————————————————————————————————————————————|*/
+/*|data:  tt_key_t left_key;                   |*/
+/*|       value_t left_value;                  |*/
+/*|       tt_key_t right_key;                  |*/
+/*|       value_t right_value;                 |*/
+/*|————————————————————————————————————————————|*/
 
 typedef unsigned char node_info_t;
-#define HEAD_SIZE (sizeof(node_info_t) + (sizeof(size_t) * 2))
-#define CHILD_P_SIZE (sizeof(void *) * 3)
-#define DATA_P_SIZE (sizeof(char *) * 2)
-#define NODE_SIZE (HEAD_SIZE + CHILD_P_SIZE + DATA_P_SIZE)
 
-#define LEFT_DATA_SIZE(node) (*(size_t *)(node + sizeof(node_info_t)))
-#define RIGHT_DATA_SIZE(node) (*(size_t *)(node + sizeof(node_info_t) + sizeof(size_t)))
-#define LEFT_DATA(node) (*(char **)(node + HEAD_SIZE + CHILD_P_SIZE))
-#define RIGHT_DATA(node) (*(char **)(node + HEAD_SIZE + CHILD_P_SIZE + sizeof(char *)))
 
-#define NODE_INFO(node) (*(node_info_t *)node)
-#define NODE_TYPE_MASK ((node_info_t)0x80) /*1000 0000*/
-#define TWO_NODE_TYPE ((node_info_t)(0 << 7))
-#define THREE_NODE_TYPE ((node_info_t)(1 << 7))
+#define NODE_TYPE_MASK ((node_info_t)0xC0) /*1100 0000*/
 
+#define TWO_NODE_TYPE ((node_info_t)(0))
+#define THREE_NODE_TYPE ((node_info_t)(1))
+#define FOUR_NODE_TYPE ((node_info_t)(2))
 #define DETAULT_NODE_TYPE TWO_NODE_TYPE
 
-#define SET_NODE_TYPE(node, type) (NODE_INFO(node) = (NODE_INFO(node) & ~NODE_TYPE_MASK) | (type))
-#define IS_TWO_NODE(node) ((NODE_INFO(node) & NODE_TYPE_MASK) < NODE_TYPE_MASK)
-#define IS_THREE_NODE(node) (!IS_TWO_NODE(node))
+#define NODE_INFO(node) (*(node_info_t *)node)
+#define SET_NODE_TYPE(node, type) (NODE_INFO(node) = (NODE_INFO(node) & ~NODE_TYPE_MASK) | (type << 6))
+#define GET_NODE_TYPE(node) ((NODE_INFO(node) & NODE_TYPE_MASK) >> 6)
+
+#define IS_TWO_NODE(node) (GET_NODE_TYPE(node) == TWO_NODE_TYPE)
+#define IS_THREE_NODE(node) (GET_NODE_TYPE(node) == THREE_NODE_TYPE)
+#define IS_FOUR_NODE(node) (GET_NODE_TYPE(node) == FOUR_NODE_TYPE)
+
+#define LEFT_KEY_SIZE(node) (*(size_t*)(node + sizeof(node_info_t)))
+#define LEFT_DATA_SIZE(node) (*(size_t *)(node + sizeof(node_info_t) + sizeof(size_t)))
+
+#define RIGHT_KEY_SIZE(node) (*(size_t*)(node + sizeof(node_info_t) + sizeof(size_t) * 2))
+#define RIGHT_DATA_SIZE(node) (*(size_t *)(node + sizeof(node_info_t) + sizeof(size_t) * 3))
+#define HEAD_SIZE (sizeof(node_info_t) + (sizeof(size_t) * 4))
+
 
 #define LEFT_CHILD(node) (*(void **)(node + HEAD_SIZE))
 #define MIDDLE_CHILD(node) (*(void **)(node + HEAD_SIZE + sizeof(void *)))
 #define RIGHT_CHILD(node) (*(void **)(node + HEAD_SIZE + sizeof(void *) * 2))
+#define TEMP_CHILD(node) (*(void **)(node + HEAD_SIZE + sizeof(void *) * 3))
+
+#define CHILD_P_SIZE (sizeof(void *) * 4)
+
+
+#define LEFT_KEY(node) (*(tt_key_t*)(node + HEAD_SIZE + CHILD_P_SIZE))
+#define LEFT_DATA(node) (*(value_t*)(node + HEAD_SIZE + CHILD_P_SIZE + sizeof(tt_key_t)))
+
+#define RIGHT_KEY(node) (*(tt_key_t*)(node + HEAD_SIZE + CHILD_P_SIZE + sizeof(tt_key_t) + sizeof(value_t)))
+#define RIGHT_DATA(node) (*(value_t*)(node + HEAD_SIZE + CHILD_P_SIZE + sizeof(tt_key_t) + sizeof(value_t) + sizeof(tt_key_t)))
+
+#define KEY_VALUE_SIZE (sizeof(tt_key_t) + sizeof(value_t))
+#define DATA_P_SIZE (KEY_VALUE_SIZE * 2)
+#define NODE_SIZE (HEAD_SIZE + CHILD_P_SIZE + DATA_P_SIZE)
+
+// ========================= macro define ===========================
+
+
 
 #define POINTER_FREE(ptr) \
     do                    \
@@ -51,6 +84,25 @@ typedef unsigned char node_info_t;
         free(ptr);        \
         ptr = NULL;       \
     } while (0)
+
+#define SET_DATA(data_ptr,data,size)    \
+    do                                  \
+    {                                   \
+        if (NULL != data_ptr)           \
+        {                               \
+            free(data_ptr);             \
+            data_ptr = NULL;            \
+        }                               \
+        data_ptr = malloc(size);        \
+        if (NULL == data_ptr)           \
+        {                               \
+            perror("malloc");           \
+            exit(EXIT_FAILURE);         \
+        }                               \
+        memcpy(data_ptr, data, size);   \
+    } while (0)
+
+// ========================= static function ===========================
 
 static inline void *new_node(void);
 static inline void free_node(void **node);
@@ -67,13 +119,20 @@ static inline void *new_node(void)
         exit(EXIT_FAILURE);
     }
     SET_NODE_TYPE(node, DETAULT_NODE_TYPE);
-    LEFT_CHILD(node) = NULL;
-    RIGHT_CHILD(node) = NULL;
-    MIDDLE_CHILD(node) = NULL;
 
+    LEFT_KEY_SIZE(node) = 0;
     LEFT_DATA_SIZE(node) = 0;
-    LEFT_DATA(node) = NULL;
+    RIGHT_KEY_SIZE(node) = 0;
     RIGHT_DATA_SIZE(node) = 0;
+
+    LEFT_CHILD(node) = NULL;
+    MIDDLE_CHILD(node) = NULL;
+    RIGHT_CHILD(node) = NULL;
+    TEMP_CHILD(node) = NULL;
+
+    LEFT_KEY(node) = NULL;
+    LEFT_DATA(node) = NULL;
+    RIGHT_KEY(node) = NULL;
     RIGHT_DATA(node) = NULL;
     return node;
 }
@@ -87,52 +146,91 @@ static inline void free_node(void **node)
     POINTER_FREE(LEFT_CHILD(*node));
     POINTER_FREE(MIDDLE_CHILD(*node));
     POINTER_FREE(RIGHT_CHILD(*node));
+    POINTER_FREE(TEMP_CHILD(*node));
+    POINTER_FREE(LEFT_KEY(*node));
     POINTER_FREE(LEFT_DATA(*node));
+    POINTER_FREE(RIGHT_KEY(*node));
     POINTER_FREE(RIGHT_DATA(*node));
     POINTER_FREE(*node);
 }
 
 static inline void set_left_data(void *node, char *data, size_t size)
 {
-    if (NULL != LEFT_DATA(node) && LEFT_DATA_SIZE(node) < size)
+    if (NULL == node || NULL == data || size == 0)
     {
-        LEFT_DATA(node) = realloc(LEFT_DATA(node), sizeof(char) * size);
-    }
-    else
-    {
-        LEFT_DATA(node) = malloc(sizeof(char) * size);
+        return;
     }
     LEFT_DATA_SIZE(node) = size;
-    for (size_t i = 0; i < size; i++)
-    {
-        LEFT_DATA(node)
-        [i] = data[i];
-    }
+    SET_DATA(LEFT_DATA(node), data, size);
 }
 
 static inline void set_right_data(void *node, char *data, size_t size)
 {
-    if (NULL != RIGHT_DATA(node) && RIGHT_DATA_SIZE(node) < size)
+    if (NULL == node || NULL == data || size == 0)
     {
-        RIGHT_DATA(node) = realloc(RIGHT_DATA(node), sizeof(char) * size);
-    }
-    else
-    {
-        RIGHT_DATA(node) = malloc(sizeof(char) * size);
+        return;
     }
     RIGHT_DATA_SIZE(node) = size;
-    for (size_t i = 0; i < size; i++)
-    {
-        RIGHT_DATA(node)
-        [i] = data[i];
-    }
+    SET_DATA(RIGHT_DATA(node), data, size);
 }
+
+
+extern tt_tree_entry *tt_tree_create(void)
+{
+    tt_tree_entry *tree = NULL;
+    tree = malloc(sizeof(tt_tree_entry));
+    if (NULL == tree)
+    {
+        perror("malloc");
+        exit(EXIT_FAILURE);
+    }
+    tree->root = new_node();
+    return tree;
+}
+extern void tt_tree_destroy(tt_tree_entry *tree)
+{
+    if (NULL == tree)
+    {
+        return;
+    }
+    free_node(&tree->root);
+    POINTER_FREE(tree);
+}
+extern void tt_tree_insert(tt_tree_entry *tree, tt_key_t key, value_t value, int (*compare)(value_t, value_t))
+{
+    if (NULL == tree || NULL == key || NULL == value || NULL == compare)
+    {
+        return;
+    }
+    void *node = NULL;
+    node = tree->root;
+}
+extern void tt_tree_delete(tt_tree_entry *tree, tt_key_t key);
+extern value_t tt_tree_search(tt_tree_entry *tree, tt_key_t key);
+
 /**
  * ======================== ▀█▀ ██▀ ▄▀▀ ▀█▀  ========================
  * ========================  █  █▄▄ ▄█▀  █   ========================
  */
-
 #ifdef _TEST_
+#include <assert.h>
+
+static inline void free_test(void)
+{
+    void *node = NULL;
+    node = new_node();
+    assert(node != NULL);
+
+    set_left_data(node, "hello", 6);
+    assert(LEFT_DATA_SIZE(node) == 6);
+    assert(LEFT_DATA(node) != NULL);
+    
+    POINTER_FREE(LEFT_DATA(node));
+    assert(LEFT_DATA(node) == NULL);
+
+    free_node(&node);
+    assert(node == NULL);
+}
 static inline void micro_test(void)
 {
     void *node = NULL;
@@ -141,13 +239,16 @@ static inline void micro_test(void)
 
     set_left_data(node, "hello", 6);
     set_right_data(node, "world", 6);
+    printf("left data size: %ld\n", LEFT_DATA_SIZE(node));
     printf("left data: %s\n", LEFT_DATA(node));
+    printf("right data size: %ld\n", RIGHT_DATA_SIZE(node));
     printf("right data: %s\n", RIGHT_DATA(node));
     free_node(&node);
     printf("node: %p\n", node);
 }
 extern void tt_tree_test(void)
 {
+    free_test();
     micro_test();
 }
 #endif
--- tt_tree.h
+++ tt_tree.h
@@ -1,12 +1,28 @@
 #ifndef _TT_TREE_H_
 #define _TT_TREE_H_
+#include <stddef.h>
+/**
+ * @brief 23查找树
+ * 有两种节点，2节点与3节点，2节点可有一个value以及两个子节点，left和right。3节点有两个value，并且可以有3个子节点，left, midle和right。
+ * 当
+ * 
+ */
 
 typedef struct
 {
-    char *node_info;
-    char *data;
+    void *root;
 
 } tt_tree_entry;
+
+typedef char *tt_key_t;
+typedef char *value_t;
+
+extern tt_tree_entry *tt_tree_create(void);
+extern void tt_tree_destroy(tt_tree_entry *tree);
+
+extern void tt_tree_insert(tt_tree_entry *tree, tt_key_t key, value_t value, int (*compare)(value_t, value_t));
+extern void tt_tree_delete(tt_tree_entry *tree, tt_key_t key);
+extern value_t tt_tree_search(tt_tree_entry *tree, tt_key_t key);
 
 
 /**
